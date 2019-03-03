ed389012d3853d52f419779963ad9f1bee26ee11
M-Leveque <leveque.melvin@gmail.com>
2019-03-03 14:39:03 +0100

    AppPotes-web : Mise en place du component Login + Mise en place du component nav-bar + Mise en place du component home-page

--- AppPotes-web/src/app/app-routing.module.ts
+++ AppPotes-web/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {LoginComponent} from './login/login.component';
+import {HomePageComponent} from './home-page/home-page.component';
 
 const routes: Routes = [
+    { path: 'login', component: LoginComponent},
+    { path: 'home-page', component: HomePageComponent},
     { path: 'not-found', component: PageNotFoundComponent },
     { path: '**', redirectTo: 'not-found' }
 ];
--- AppPotes-web/src/app/app.module.ts
+++ AppPotes-web/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LoginComponent } from './login/login.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    LoginComponent,
+    NavBarComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
