9184531ed7baa5acabd6b9ae23095c96526558d4
LÉVÊQUE <melvin.leveque@cgi.com>
2019-03-07 13:58:08 +0100

    Angular : homePage : Mise en place de l'affichage dynamique des sections

--- AppPotes-web/src/app/album/album.component.ts
+++ AppPotes-web/src/app/album/album.component.ts
@@ -7,24 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlbumComponent implements OnInit {
 
-  public albums : Array<object>;
-
-  constructor() { 
-    this.albums = [
-      {
-        "titre" : "Album1",
-        "src"   : "img01.png"
-      },
-      {
-        "titre" : "Album2",
-        "src"   : "img02.png"
-      },
-      {
-        "titre" : "Album3",
-        "src"   : "img03.png"
-      }
-    ];
-  }
+  constructor() {}
 
   ngOnInit() {
   }
--- AppPotes-web/src/app/home-page/home-page.component.ts
+++ AppPotes-web/src/app/home-page/home-page.component.ts
@@ -7,6 +7,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
+  albums = [
+    {
+      title: 'Randonnée',
+      path: 'montagne.jpg'
+    },
+    {
+      title: 'Concert',
+      path: 'pink-floyd.jpg'
+    },
+    {
+      title: 'Vancance 2016',
+      path: 'plage.jpg'
+    }
+  ];
+
+  events = [
+    {
+      title: 'Event1',
+      description: 'Event1 description',
+      icon: '🎁',
+      date: '12-05-2019'
+    },
+    {
+      title: 'Event2',
+      description: 'Event2 description',
+      icon: '👍',
+      date: '13-06-2019'
+    },
+    {
+      title: 'Event3',
+      description: 'Event3 description',
+      icon: '🥐',
+      date: '14-07-2019'
+    }
+  ];
+
+  pools = [
+    {
+      title: 'Pool1',
+      description: 'Pool1 description',
+      cash: 100,
+      endDate: '14-07-2021'
+    },
+    {
+      title: 'Pool2',
+      description: 'Pool3 description',
+      cash: 50,
+      endDate: '14-07-2022'
+    },
+    {
+      title: 'Pool3',
+      description: 'Pool3 description',
+      cash: 25,
+      endDate: '14-07-2023'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
