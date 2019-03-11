6cbeeeb183334ae9986399b6dff779a266f84137
LÉVÊQUE <melvin.leveque@cgi.com>
2019-03-11 13:59:33 +0100

    Angular : Component Album : trie des albums par date et par titre
    Signed-off-by: LÉVÊQUE <melvin.leveque@cgi.com>

--- AppPotes-web/src/app/album/list/album-list.component.ts
+++ AppPotes-web/src/app/album/list/album-list.component.ts
@@ -9,30 +9,54 @@ export class AlbumListComponent implements OnInit {
   sharedAlbum = {
     id: 0,
     title: 'Album commun',
-    path: 'vrac.jpg'
+    path: 'vrac.jpg',
+    date: '2015-06-15'
   }
 
   albums = [
     {
       id: 1,
       title: 'Randonnée',
-      path: 'montagne.jpg'
+      path: 'montagne.jpg',
+      date: '2015-06-14'
     },
     {
       id: 2,
       title: 'Concert',
-      path: 'pink-floyd.jpg'
+      path: 'pink-floyd.jpg',
+      date: '2015-06-13'
     },
     {
       id: 3,
       title: 'Vancance 2016',
-      path: 'plage.jpg'
+      path: 'plage.jpg',
+      date: '2015-06-12'
     }
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public sortByTitle(){
+    this.albums.sort( (a, b) => {
+      if( a.title > b.title )
+        return 1;
+      if( a.title < b.title)
+        return -1;
+      return 0
+    });
+  }
+
+  public sortByDate(){
+    this.albums.sort( (a, b) => {
+      if( a.date > b.date )
+        return 1;
+      if( a.date < b.date)
+        return -1;
+      return 0
+    });
   }
 
 }
