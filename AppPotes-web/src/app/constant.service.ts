76f3efc7d6204bcbd1329fbbda5c5ee489e33bdf
CaptainAd-Hoc <captainad-h0c@protonmail.com>
2020-12-16 11:47:59 +0100

    Add environement lunch

--- AppPotes-web/src/app/constant.service.ts
+++ AppPotes-web/src/app/constant.service.ts
@@ -1,9 +1,9 @@
-import { User } from './models/User.model';
+import { environment } from './../environments/environment';
 
 export class ConstantService {
 
     TEXT_FIELD_PATTERN: string = "^[A-Za-zÀ-ÖØ-öø-ÿ0-9 '_-]+$";
-    host: string = "http://localhost:8000/";
+    host: string = environment.apiUrl;
     path: Object = {
         "albums" : "storage/img/albums/"
     }
