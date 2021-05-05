8fbc03f0a617b9184e363cabd3f2fb4c1644f197
M-Leveque <captainad-h0c@protonmail.com>
2021-05-05 08:45:52 +0200

    [DEV] Fix build angular

--- AppPotes-web/src/app/models/User.model.ts
+++ AppPotes-web/src/app/models/User.model.ts
@@ -4,8 +4,8 @@ export class User{
     constructor(
         public id: Number,
         public name: String,
-        public description: String,
         public email: String,
+        public description: String = null,
         public password: String = null,
         public token: String = null,
         public photo : Photo = null,
